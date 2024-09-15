56a68a423b010aef63e4d4f25fbb79f16ba29035
tuan.hv <tuan.hv@hodfords.com>
2024-09-15 14:29:02 +0700

    update-ui

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,5 +4,7 @@ import { test } from "@playwright/test";
 test("Percy", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.waitForLoadState();
-    await percySnapshot(page, "Sign In Page");
+    await percySnapshot(page, "Sign In Page", {
+        percyCSS: "div h1 {color:red;}",
+    });
 });
